dc32b3c82d03ed5c31fc1017ef90815aae635cb1
mark.a.cabardo <mark.a.cabardo@accenture.com>
2019-03-13 17:46:19 +0800

    commit

---
+++ src/main/java/com.demo.examples/MessageBuilder.java
@@ -0,0 +1,13 @@
+package com.demo.examples;
+
+public class MessageBuilder {
+
+    public static String getHelloWorld(){
+        return "hello world";
+    }
+
+    public static int getNumber10(){
+        return 10;
+    }
+
+}
